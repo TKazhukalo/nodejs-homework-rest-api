925ea337571cc380365e729139b50484c685ad2b
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-08-19 23:56:59 +0300

    create task03-mongodb

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
+const dotenv = require("dotenv");
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
+dotenv.config()
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,10 +1,10 @@
-const Joi = require("joi");
+// const Joi = require("joi");
 
-const addSchema = Joi.object({
-    name: Joi.string().trim().min(2).pattern(/^[a-zA-Zа-яА-Я\s\-']+$/).required(),
-    email: Joi.string().email().required(),
-    phone:Joi.string().required().pattern(/^\d{10}$/),
-})
+// const addSchema = Joi.object({
+//     name: Joi.string().trim().min(2).pattern(/^[a-zA-Zа-яА-Я\s\-']+$/).required(),
+//     email: Joi.string().email().required(),
+//     phone:Joi.string().required().pattern(/^\d{10}$/),
+// })
 
 module.exports = {
     addSchema,
